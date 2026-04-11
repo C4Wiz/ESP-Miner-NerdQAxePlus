cbd07f5358c906ea4155319fa39dd4843529d94e
C4Wiz <c4wiz@live.com>
2026-04-11 12:36:29 -0500

    Max overclock 950Mhz, Additional fine control of freq and voltage

--- main/boards/nerdqaxeplus2.cpp
+++ main/boards/nerdqaxeplus2.cpp
@@ -13,11 +13,11 @@ NerdQaxePlus2::NerdQaxePlus2() : NerdQaxePlus() {
     m_ifault = (float) (m_imax + 5);
 
     m_asicJobIntervalMs = 500;
-    m_asicFrequencies = {500, 515, 525, 550, 575, 590, 600};
-    m_asicVoltages = {1120, 1130, 1140, 1150, 1160, 1170, 1180, 1190, 1200};
+    m_asicFrequencies = {600, 615, 625, 650, 675, 690, 700, 715, 725, 750, 775, 790, 800, 805, 810, 815, 820, 825, 830, 835, 840, 845, 850, 855, 860, 865, 870, 875, 880, 885, 890, 895, 900, 905, 910, 915, 920, 925, 930, 935, 940, 945, 950};
+    m_asicVoltages = {1150, 1160, 1170, 1180, 1190, 1200, 1210, 1215, 1220, 1225, 1230, 1235, 1240, 1245, 1250, 1255, 1260, 1265, 1270, 1275, 1280, 1285, 1290, 1295, 1300, 1305, 1310, 1315, 1320, 1325, 1330, 1335, 1340, 1345, 1350, 1355, 1360, 1365, 1370, 1375, 1380, 1385, 1390, 1395, 1400};
     m_defaultAsicFrequency = m_asicFrequency = 600; // default frequency
     m_defaultAsicVoltageMillis = m_asicVoltageMillis = 1150; // default voltage
-    m_absMaxAsicFrequency = 925; // max overclock 925
+    m_absMaxAsicFrequency = 950; // max overclock 950
     m_absMaxAsicVoltageMillis = 1400;
     m_initVoltageMillis = 1200;
     
